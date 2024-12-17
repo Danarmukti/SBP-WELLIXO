dcb3b5ee4aa7d7e09a884f65f7de052a2a90318b
DanarCode <Danarmukti@users.noreply.github.com>
2024-12-18 05:32:23 +0700

    formula added

--- src/page/calculator-page/config.js
+++ src/page/calculator-page/config.js
@@ -11,13 +11,15 @@ const moderate = document.getElementById("moderatebtn");
 const actifity = document.getElementById("activebtn");
 const calculate = document.getElementById("calculateCalorie");
 
-const metricmeter = document.getElementById("usmeter");
-const usmeter = document.getElementById("metricmeter");
+const usmeter = document.getElementById("usmeter");
+const metricmeter = document.getElementById("metricmeter");
+const poundmeter = document.getElementById("poundmeter");
+const kgmeter = document.getElementById("kgmeter");
 
 //calculate var
 let unitsmeter = "us";
-let gendereveal = "";
-let actifityname = "";
+let gendereveal = "men";
+let actifityname = "sedentary";
 
 // choose gender
 men.addEventListener("click", () => {
@@ -26,17 +28,6 @@ men.addEventListener("click", () => {
 // choose gender
 female.addEventListener("click", () => {
   gendereveal = "female";
-});
-
-//choose actifity level
-sedentary.addEventListener("click", () => {
-  actifityname = "sedentary";
-});
-moderate.addEventListener("click", () => {
-  actifityname = "moderate";
-});
-actifity.addEventListener("click", () => {
-  actifityname = "active";
 });
 
 // UNITS INTERACTIVE
@@ -48,13 +39,16 @@ us.addEventListener("click", () => {
   unitToggle1.style.left = "0px";
   unitToggle1.style.transition = "0.3s all ease-in-out";
 
-  usmeter.classList.add("d-none");
   if ((usmeter.classList.contains = "d-none")) {
     usmeter.classList.remove("d-none");
     metricmeter.classList.add("d-none");
+    poundmeter.classList.remove("d-none");
+    kgmeter.classList.add("d-none");
   } else {
     usmeter.classList.add("d-none");
     metricmeter.classList.remove("d-none");
+    poundmeter.classList.add("d-none");
+    kgmeter.classList.remove("d-none");
   }
   unitsmeter = "us";
 });
@@ -68,9 +62,13 @@ metric.addEventListener("click", () => {
 
   if ((metricmeter.classList.contains = "d-none")) {
     //find if class have "d-none" class
+    kgmeter.classList.remove("d-none");
+    poundmeter.classList.add("d-none");
     metricmeter.classList.remove("d-none"); //if have it will be remove
     usmeter.classList.add("d-none"); //and this will be added
   } else {
+    kgmeter.classList.add("d-none");
+    poundmeter.classList.remove("d-none");
     metricmeter.classList.add("d-none"); //if don't it will added
     usmeter.classList.remove("d-none"); // and this will be deleted
   }
@@ -107,6 +105,9 @@ sedentary.addEventListener("click", () => {
   unitToggle3.style.width = "145px";
   unitToggle3.style.left = "0";
   unitToggle3.style.transition = "0.3s all ease-in-out";
+  //choose actifity level
+
+  actifityname = "sedentary";
 });
 
 // press button unit
@@ -115,6 +116,9 @@ moderate.addEventListener("click", () => {
   unitToggle3.style.width = "40%";
   unitToggle3.style.left = "125px";
   unitToggle3.style.transition = "0.3s all ease-in-out";
+  //choose actifity level
+
+  actifityname = "moderate";
 });
 
 // press button unit
@@ -123,6 +127,9 @@ actifity.addEventListener("click", () => {
   unitToggle3.style.width = "30%";
   unitToggle3.style.left = "250px";
   unitToggle3.style.transition = "0.3s all ease-in-out";
+  //choose actifity level
+
+  actifityname = "active";
 });
 // ACTIVITY INTERACTIVE
 
@@ -143,11 +150,81 @@ calculate.addEventListener("click", function () {
     }
   }
   if (unitsmeter === "metric") {
-    if (kg.value == "" && cm.value == "" && age.value == "") {
+    if (pounds.value == "" && cm.value == "" && age.value == "") {
       alert("masukan angka");
       console.log("metric");
     }
   }
+  bmrCalculate();
+  tdee();
 });
 
+let bmrResult;
+
+//formula metric
+/*  Pria:
+BMR= 10×berat badan(kg)+6.25×tinggi badan (cm)−5×usia (tahun)+5
+
+Wanita:
+BMR=10×berat badan (kg)+6.25×tinggi badan (cm)−5×usia (tahun)−161
+
+*/
+//formula us
+/*  Pria:
+BMR=66+(6.23×berat (lb))+(12.7×tinggi (in))−(6.8×usia)
+
+Wanita:
+BMR=655+(4.35×berat (lb))+(4.7×tinggi (in))−(4.7×usia)
+
+*/
+
+function bmrCalculate() {
+  if (unitsmeter == "us") {
+    if (gendereveal == "men") {
+      bmrResult = 66 + 6.23 * pounds.value + 12.7 * inch - 6.8 * age.value;
+      console.log(bmrResult);
+    }
+  }
+  if (unitsmeter == "metric") {
+    if (gendereveal == "men") {
+      bmrResult = 10 * kg.value + 6.25 * cm.value - 5 * age.value + 5;
+      console.log(bmrResult);
+    }
+  }
+  if (unitsmeter == "us") {
+    if (gendereveal == "female") {
+      bmrResult = 655 + 6.23 * pounds.value + 4.7 * inch - 4.7 * age.value;
+      console.log(bmrResult);
+    }
+  }
+  if (unitsmeter == "metric") {
+    if (gendereveal == "female") {
+      bmrResult = 10 * kg.value + 6.25 * cm.value - 5 * age.value - 161;
+      console.log(bmrResult);
+    }
+  }
+  return bmrResult;
+}
+
+let tdeeResult;
+
+function tdee() {
+  if (actifityname == "sedentary") {
+    tdeeResult = bmrResult * 1.2;
+    console.log(tdeeResult);
+  } else if (actifityname == "moderate") {
+    tdeeResult = bmrResult * 1.55;
+    console.log(tdeeResult);
+  } else if (actifityname == "active") {
+    tdeeResult = bmrResult * 1.9;
+    console.log(tdeeResult);
+  }
+  return tdeeResult;
+}
+
+function bmiCalculate() {
+  if (unitsmeter == "us") {
+  } else {
+  }
+}
 // CALCULATE
