cbd349f2d8cf94c48141df09b3e7fb317f4ec2af
DanarCode <Danarmukti@users.noreply.github.com>
2024-12-18 01:32:10 +0700

    interactive button units, gender, activity button

---
+++ src/page/calculator-page/config.js
@@ -0,0 +1,83 @@
+// press us button unit
+const us = document.getElementById("usUnits");
+const unitToggle1 = document.querySelector(".btn-switch-toggle1");
+const unitToggle2 = document.querySelector(".btn-switch-toggle2");
+const unitToggle3 = document.querySelector(".btn-switch-toggle3");
+const metric = document.getElementById("metricUnits");
+const men = document.getElementById("men");
+const female = document.getElementById("female");
+const sedentary = document.getElementById("sedentarybtn");
+const moderate = document.getElementById("moderatebtn");
+const actifity = document.getElementById("activebtn");
+
+//calculate var
+let unitsmeter = "";
+let gendereveal = "";
+let actifityname = "";
+
+// UNITS INTERACTIVE
+
+// when us unit click
+// when us unit click then indicator move into us
+us.addEventListener("click", () => {
+  unitToggle1.style.width = "80px";
+  unitToggle1.style.left = "0px";
+  unitToggle1.style.transition = "0.3s all ease-in-out";
+});
+
+// press metric button unit
+// when  unit click then indicator move into metric
+metric.addEventListener("click", () => {
+  unitToggle1.style.width = "110px";
+  unitToggle1.style.left = "75px";
+  unitToggle1.style.transition = "0.3s all ease-in-out";
+});
+
+// UNITS INTERACTIVE
+
+// GENDER INTERACTIVE
+
+// press button unit
+// when men unit click then indicator move into men
+men.addEventListener("click", () => {
+  unitToggle2.style.width = "100px";
+  unitToggle2.style.left = "0px";
+  unitToggle2.style.transition = "0.3s all ease-in-out";
+});
+
+// press button unit
+// when Female unit click then indicator move into female
+female.addEventListener("click", () => {
+  unitToggle2.style.width = "110px";
+  unitToggle2.style.left = "95px";
+  unitToggle2.style.transition = "0.3s all ease-in-out";
+});
+
+// GENDER INTERACTIVE
+
+// ACTIVITY INTERACTIVE
+
+// press button unit
+// when sedentary unit click then indicator move into sedentary
+sedentary.addEventListener("click", () => {
+  unitToggle3.style.width = "145px";
+  unitToggle3.style.left = "0";
+  unitToggle3.style.transition = "0.3s all ease-in-out";
+});
+
+// press button unit
+// when moderate unit click then indicator move into moderate
+moderate.addEventListener("click", () => {
+  unitToggle3.style.width = "145px";
+  unitToggle3.style.left = "123px";
+  unitToggle3.style.transition = "0.3s all ease-in-out";
+});
+
+// press button unit
+// when actifity unit click then indicator move into actifity
+actifity.addEventListener("click", () => {
+  unitToggle3.style.width = "110px";
+  unitToggle3.style.left = "255px";
+  unitToggle3.style.transition = "0.3s all ease-in-out";
+});
+// ACTIVITY INTERACTIVE
